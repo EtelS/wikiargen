caaa77d97ff85dfc4566d8a7f64204f8de2be625
Perez Etel S <etelpe@gmail.com>
2025-12-31 11:20:11 -0300

    agregado de documentos

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,18 @@
-
+import { Documentos } from "../data/documents"
 
 export function List () {
+  console.log(Documentos)
   return (
     <>
     <h2>Preguntas frecuentes</h2>
-    <ul>
-        <li>1 </li>
-        <li>1 </li>
-    </ul>
+    <div>
+      {Documentos.map(doc=>(
+        <div key={doc.id}>
+          <a href={doc.url} target='_blank'>{doc.title}</a>
+        </div>
+      ))
+    }
+    </div>
     </>
   )
 }
---
+++ src/data/documents.ts
@@ -0,0 +1,33 @@
+export const Documentos=[
+    {
+        id:'1',
+        title: 'Cómo se loguea el whats app del cliente a nuestra plataforma',
+        url:'https://www.dropbox.com/scl/fi/qwraocn49qnbhq8w4315h/Instructivo-Whatsapp-Propio.pdf?rlkey=4y2jxblr2vx4bzg6jpc99eaxp&st=5sjlt9jd&dl=0'
+    },
+     {
+        id:'2',
+        title: 'Cómo agregar informes medicos en Argensalud',
+        url:'https://www.dropbox.com/scl/fi/3yzmh275vvligpeea7yr8/Como-agregar-informes-medicos.pdf?rlkey=fj9256mkuzohiyqbpe00zv3he&st=lr9j8wjt&dl=0'
+    },
+    {
+        id:'3',
+        title: 'Agregar comprobantes de proveedores',
+        url:'https://www.dropbox.com/scl/fi/4b97d0jnrg82zkzz5ejzj/Agregar-comprobantes-externos.pdf?rlkey=dwogc0hmixb6yb3kolmu898x9&st=9qx4iozq&dl=0'
+    },
+    {
+        id:'4',
+        title: 'Configuración de turnos para videollamada',
+        url:'https://www.dropbox.com/scl/fi/2qxnuw6nbb85hs223vzrn/configurar-turnos-para-videollamada.pdf?rlkey=knj59fp3gz5tj6jr1f0hmunmc&st=jzog81cl&dl=0'
+    },
+    {
+        id:'5',
+        title: 'Categorias de turnos',
+        url:'https://www.dropbox.com/scl/fi/l1m6jkprqi3in4dankdo8/Categorias-de-turnos.pdf?rlkey=4p5s3az80cv1zwmmuecn9mh20&st=pierql79&dl=0'
+    },
+    {
+        id:'6',
+        title: 'Configurar el llamador y llamar desde un consultorio',
+        url:'https://www.dropbox.com/scl/fi/x0rjmk4krrsfcylpgzpno/Como-configurar-el-llamador-y-llamar.pdf?rlkey=xp6uihphouv1qw77xavsq9gwk&st=28qmpwvg&dl=0'
+    },
+]
+
